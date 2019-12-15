2bb78e172928cdddd8d91103733af9eb1e7d988d
Yash Patel <ypatel5400@gmail.com>
2019-12-15 09:25:37 -0800

    Completed day 1 part 2

--- day1.py
+++ day1.py
@@ -1,3 +1,16 @@
 import os
-ans = sum(int(l) // 3 - 2 for l in open(os.path.expanduser("~/Downloads/input.txt")))
+cached = {}
+masses = [int(l) for l in open(os.path.expanduser("~/Downloads/input.txt"))]
+ans = 0
+
+while len(masses) > 0:
+    mass = masses.pop()
+    if mass in cached:
+        new_mass = cached[mass]
+    else:
+        new_mass = int(mass) // 3 - 2
+    if new_mass > 0:
+        cached[mass] = new_mass
+        ans += new_mass
+        masses.append(new_mass)
 print(ans)
